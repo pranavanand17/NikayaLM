e75af3f48e6311e328180b15043f92b19057d21c
pranavanand17 <pranavisappu@gmail.com>
2025-01-04 18:42:24 +0530

    venv

--- Nikaya/app/models.py
+++ Nikaya/app/models.py
@@ -4,24 +4,4 @@ from django.db import models
 class CustomUser(AbstractUser):
     is_admin= models.BooleanField(default=False)
 
-class Book(models.Model):
-    id=models.AutoField(primary_key=True)
-    name=models.CharField(max_length=255)
-    image=models.ImageField(upload_to='book_images'/)
-    quantity=models.PositiveIntegerField(default=0)
-
-    def __str__(self):
-        return self.name
-    def borrow(self):
-        """Reduce the quantity by 1 when borrowed."""
-        if self.quantity>0:
-            self.quantity-=1;
-            self.save()
-        else:
-            raise ValueError("No copies left to borrow!")
-
-    def return_book(self):
-        """Increase the quantity by 1 when returned."""
-        self.quantity+=1
-        self.save()
 
